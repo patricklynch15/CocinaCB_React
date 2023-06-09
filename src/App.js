a348b4ac95a5cb41d9c2a80b3cc842cff816d6fe
patricklynch15 <69437363+patricklynch15@users.noreply.github.com>
2023-06-09 14:55:00 -0400

    Restructuring folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+//import './Home Page.html';
 
 function App() {
   return (
-    <div className="App">
+    //<!DOCTYPE html> //why don't I need this?
+
+    <html lang="en">
+
+    <head>
+      <meta charset="UTF-8" />
+      <link rel="stylesheet" href="CocinaCB.css" type="text/css" />
+    </head>
+    <body className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,11 +23,31 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
+        > Home
+        </a>        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > Gallery
+        </a>        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > Menu
+        </a>        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        > Order Now!
         </a>
       </header>
-    </div>
+    
+    </body>
+    </html>
+
   );
 }
 
