15ae7377ead8d85cfb1989cd79b97795c61ed71f
patricklynch15 <69437363+patricklynch15@users.noreply.github.com>
2023-06-30 23:46:15 -0400

    Resubmitting changes
     added 4:5 ratio photos for menu tiles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-//import './Home Page.html';
 
 function App() {
   return (
-    //<!DOCTYPE html> //why don't I need this?
-
     <html lang="en">
 
     <head>
@@ -13,42 +10,57 @@ function App() {
       <link rel="stylesheet" href="CocinaCB.css" type="text/css" />
     </head>
     <body className="App">
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo"/>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {/* <img src={logo} className="App-logo" alt="logo"/> */}
+        <p>Cocina Cibaeña &emsp; &emsp;&emsp; &emsp;&emsp; &emsp; <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noopener noreferrer"> Home (React Quick Start) </a> &emsp;
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Gallery </a> &emsp;    
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Menu</a> &emsp;
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Order Now! </a> &emsp;
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > Home
-        </a>        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > Gallery
-        </a>        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > Menu
-        </a>        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        > Order Now!
-        </a>
       </header>
     
+      <table>
+        <tr>
+          <td>
+            <img src={require("./Food Pics/Spread 1.jpeg")} alt="Fish 3"/>
+          </td>
+          <td>
+            <img src={require("./Food Pics/Pork 2.jpeg")} alt="Fish 3"/>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src={require("./Food Pics/Fish 8.jpeg")} alt="Fish 3"/>
+          </td>
+          <td>
+            <img src={require("./Food Pics/Plate 3.jpeg")} alt="Fish 1"/>
+          </td>
+        </tr>
+
+      </table>
+
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+       
+
     </body>
     </html>
 
   );
 }
+
+function MyDropdown() {
+  return (
+    <menu id="Dropdown">Soon to be Dropdown menu</menu>
+  );
+}
+
+// function Slideshow() {
+
+//   return;
+// }
 
 export default App;
