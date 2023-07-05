811a1acc4cd6bbc0e94d52e3e36624c2047d9da5
patricklynch15 <69437363+patricklynch15@users.noreply.github.com>
2023-07-05 12:06:46 -0400

    Adding images and making edits to App.js
    Adding images for main menu grid.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
+import './About-Us.html';
 
 function App() {
   return (
@@ -7,39 +8,39 @@ function App() {
 
     <head>
       <meta charset="UTF-8" />
-      <link rel="stylesheet" href="CocinaCB.css" type="text/css" />
     </head>
     <body className="App">
 
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo"/> */}
-        <p>Cocina Cibaeña &emsp; &emsp;&emsp; &emsp;&emsp; &emsp; <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noopener noreferrer"> Home (React Quick Start) </a> &emsp;
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Gallery </a> &emsp;    
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Menu</a> &emsp;
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Order Now! </a> &emsp;
+    <CocinaCBHeader></CocinaCBHeader>
+      
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
 
-        </p>
-      </header>
-    
-      <table>
+
+      <center>
+      <table class="mainPageDisplay">
         <tr>
           <td>
-            <img src={require("./Food Pics/Spread 1.jpeg")} alt="Fish 3"/>
+            <img src={require("./Food Pics/Main Menu 3 45 ratio.jpeg")} class="blue-flag-square" alt="main dish 1"/>
           </td>
           <td>
-            <img src={require("./Food Pics/Pork 2.jpeg")} alt="Fish 3"/>
+            <img src={require("./Food Pics/Main Menu 1 45 ratio.jpeg")} class="red-flag-square" alt="main dish 2"/>
           </td>
         </tr>
         <tr>
           <td>
-            <img src={require("./Food Pics/Fish 8.jpeg")} alt="Fish 3"/>
+            <img src={require("./Food Pics/Main Menu 4 45 ratio.jpeg")} class="red-flag-square" alt="main dish 3"/>
           </td>
           <td>
-            <img src={require("./Food Pics/Plate 3.jpeg")} alt="Fish 1"/>
+            <img src={require("./Food Pics/Main Menu 6 45 ratio.jpeg")} class="blue-flag-square" alt="main dish 4"/>
           </td>
         </tr>
 
       </table>
+      </center>
+      
 
       <p>
         Edit <code>src/App.js</code> and save to reload.
@@ -52,15 +53,20 @@ function App() {
   );
 }
 
-function MyDropdown() {
-  return (
-    <menu id="Dropdown">Soon to be Dropdown menu</menu>
-  );
+
+
+function CocinaCBHeader() {
+  return (<header className="App-header">
+  {/* <img src={logo} className="App-logo" alt="logo"/> */}
+  <p>Cocina Cibaeña &emsp; &emsp; &emsp; &emsp; &emsp; &emsp; <a className="App-link" href="App.js" target="_blank" rel="noopener noreferrer"> Home (React Quick Start) </a> &emsp;
+  <a className="App-link" href="https://reactjs.org"> Gallery </a> &emsp;    
+  <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Menu</a> &emsp;
+  <a className="App-link" href="About-Us.html" target="_blank"> Learn About Us</a> &emsp;
+  <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noopener noreferrer"> Order Now! </a> &emsp;
+
+  </p>
+</header>);
 }
 
-// function Slideshow() {
-
-//   return;
-// }
 
 export default App;
